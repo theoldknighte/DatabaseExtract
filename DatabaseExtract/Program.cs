218ed6052907cae76786f0e968613690a2bf762f
Adam Murray <Adam.k.murray@gmail.com>
2024-07-12 13:08:03 -0500

    Refactoring to pull parsing data out from main method.

--- DatabaseExtract/Program.cs
+++ DatabaseExtract/Program.cs
@@ -15,6 +15,7 @@ internal class Program
             Console.ReadLine();
             throw new ArgumentException("Must provide output path");
         }
+
         string pathToDesiredOutputLocation = Path.GetFullPath(args[0]);
         //string pathToDesiredOutputLocation = @"C:\DatabaseTest\DefaultArgsOutput.csv";
         SQLiteConnection connection = null;
@@ -28,50 +29,13 @@ internal class Program
                                            "FROM Invoice invoice " +
                                            "JOIN InvoiceLine line ON invoice.InvoiceId = line.InvoiceLineId " +
                                            "WHERE invoice.BillingState is null";
-                //GetSchemaInfo(connection, sqlQuery);
-
-                //var transaction = connection.BeginTransaction();
 
                 SQLiteCommand command = connection.CreateCommand();
                 command.CommandText = sqlQuery;
                 var reader = command.ExecuteReader(System.Data.CommandBehavior.Default);
-                var schema = reader.GetSchemaTable();
+                var ColumnsAndRows = ParseDataFromReader(reader);
 
-                List<string> columns = new List<string>();
-                string currentColumn = string.Empty;
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    currentColumn = reader.GetName(i);
-                    Console.WriteLine($"Current column: {currentColumn}");
-                    columns.Add(currentColumn);
-                }
-                List<List<string>> rows = new List<List<string>>();
-                List<string> currentRow = null;
-                string temp = string.Empty;
-                while (reader.Read())
-                {
-                    currentRow = new List<string>();
-                    rows.Add(currentRow);
-                    for (int i = 0; i < columns.Count; i++)
-                    {
-                        temp = reader.GetValue(i).ToString();
-                        Console.Write($"{temp}");
-                        if(i !=  columns.Count - 1)
-                        {
-                            Console.Write(", ");
-                        }
-                        currentRow.Add(temp);
-                    }
-                    Console.WriteLine();
-                }
-
-                WriteCSV(columns, rows, pathToDesiredOutputLocation);
-
-                //foreach (var row in reader)
-                //{
-                //    string rowValue = row.ToString();
-                //    Console.WriteLine(rowValue);
-                //}
+                WriteCSV(ColumnsAndRows.Item1, ColumnsAndRows.Item2, pathToDesiredOutputLocation);
             }
         }
         
@@ -79,15 +43,47 @@ internal class Program
         {
             Console.WriteLine($"Exception throw: {ex.ToString()}");
         }
-        //finally
-        //{
-        //    if (connection != null && connection.State == ConnectionState.Open)
-        //    {
-        //        connection.Close();
-        //    }
-        //}
 
         Console.ReadLine();
+    }
+
+    private static Tuple<List<string>, List<List<string>>> ParseDataFromReader(SQLiteDataReader dataReader)
+    {
+        if(dataReader == null)
+        {
+            throw new ArgumentException($"{nameof(dataReader)} cannot be null");
+        }
+        var schema = dataReader.GetSchemaTable();
+
+        List<string> columns = new List<string>();
+        string currentColumn = string.Empty;
+        for (int i = 0; i < dataReader.FieldCount; i++)
+        {
+            currentColumn = dataReader.GetName(i);
+            Console.WriteLine($"Current column: {currentColumn}");
+            columns.Add(currentColumn);
+        }
+        List<List<string>> rows = new List<List<string>>();
+        List<string> currentRow = null;
+        string temp = string.Empty;
+        while (dataReader.Read())
+        {
+            currentRow = new List<string>();
+            rows.Add(currentRow);
+            for (int i = 0; i < columns.Count; i++)
+            {
+                temp = dataReader.GetValue(i).ToString();
+                Console.Write($"{temp}");
+                if (i != columns.Count - 1)
+                {
+                    Console.Write(", ");
+                }
+                currentRow.Add(temp);
+            }
+            Console.WriteLine();
+        }
+
+        return new Tuple<List<string>, List<List<string>>>(columns, rows);
     }
 
     private static void WriteCSV(List<string> columns, List<List<string>> rows, string outputLocation)
@@ -115,7 +111,9 @@ internal class Program
                     streamWriter.Write(",");
                 }
             }
+
             streamWriter.WriteLine();
+
             foreach(List<string> row in rows)
             {
                 for (int i = 0; i < row.Count; i++)
@@ -137,25 +135,20 @@ internal class Program
 
     static void GetSchemaInfo(SQLiteConnection connection, string sqlQuery)
     {
-        //using (connection)
-        //{
-            SQLiteCommand command = new SQLiteCommand(
-                                                      sqlQuery,
-                                                      connection);
-            //connection.Open();
+        SQLiteCommand command = new SQLiteCommand( sqlQuery,
+                                                   connection);
 
-            SQLiteDataReader reader = command.ExecuteReader();
-            DataTable schemaTable = reader.GetSchemaTable();
+        SQLiteDataReader reader = command.ExecuteReader();
+        DataTable schemaTable = reader.GetSchemaTable();
 
-            foreach (DataRow row in schemaTable.Rows)
+        foreach (DataRow row in schemaTable.Rows)
+        {
+            Console.WriteLine("New Data Row \r\n");
+            foreach (DataColumn column in schemaTable.Columns)
             {
-                Console.WriteLine("New Data Row \r\n");
-                foreach (DataColumn column in schemaTable.Columns)
-                {
-                    Console.WriteLine(string.Format("{0} = {1}",
-                       column.ColumnName, row[column]));
-                }
+                Console.WriteLine(string.Format("{0} = {1}",
+                    column.ColumnName, row[column]));
             }
-        //}
+        }
     }
 }
