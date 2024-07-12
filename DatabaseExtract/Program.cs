227142732214b755c11018e63739179a1dbd9ee3
ejber <ejber@DESKTOP-8748QJ1>
2024-07-12 10:42:20 -0500

    Add project files.

---
+++ DatabaseExtract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseExtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
