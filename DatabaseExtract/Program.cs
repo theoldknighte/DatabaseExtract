0566bfb575cb89b2f286bdf1affa63af9e25f651
Adam Murray <Adam.k.murray@gmail.com>
2024-07-12 12:01:29 -0500

    Working demo of reading SQL

--- DatabaseExtract/Program.cs
+++ DatabaseExtract/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.CodeDom;
+using System.Data.SqlClient;
+using System.Data;
 using System.Data.SQLite;
 
 namespace DatabaseExtract;
@@ -11,39 +13,94 @@ internal class Program
         try
         {
             connection = new SQLiteConnection("Data Source=C:\\Users\\ejber\\AppData\\Roaming\\LINQPad\\ChinookDemoDb.sqlite");
-            connection.Open();
-            var transaction = connection.BeginTransaction();
+            using (connection)
+            {
+                connection.Open();
+                string sqlQuery = "SELECT * " +
+                                           "FROM Invoice invoice " +
+                                           "JOIN InvoiceLine line ON invoice.InvoiceId = line.InvoiceLineId " +
+                                           "WHERE invoice.BillingState is null";
+                //GetSchemaInfo(connection, sqlQuery);
 
-            SQLiteCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT * " +
-                                  "FROM Invoice invoice " + 
-                                  "JOIN InvoiceLine line ON invoice.InvoiceId = line.InvoiceLineId " +
-                                  "WHERE invoice.BillingState is null";
-            var reader = command.ExecuteReader(System.Data.CommandBehavior.Default);
-            List<string> columns = new List<string>();
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                columns.Add(reader.GetName(i));
-            }
-            foreach (var row in reader)
-            {
-                string rowValue = row.ToString();
-                Console.WriteLine(rowValue);
+                //var transaction = connection.BeginTransaction();
+
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandText = sqlQuery;
+                var reader = command.ExecuteReader(System.Data.CommandBehavior.Default);
+                var schema = reader.GetSchemaTable();
+
+                List<string> columns = new List<string>();
+                string currentColumn = string.Empty;
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    currentColumn = reader.GetName(i);
+                    Console.WriteLine($"Current column: {currentColumn}");
+                    columns.Add(currentColumn);
+                }
+                List<string> ouput = new List<string>();
+                string temp = string.Empty;
+                while (reader.Read())
+                {
+                    for (int i = 0; i < columns.Count; i++)
+                    {
+                        temp = reader.GetValue(i).ToString();
+                        Console.Write($"{temp}");
+                        if(i !=  columns.Count - 1)
+                        {
+                            Console.Write(", ");
+                        }
+                        ouput.Add(temp);
+                    }
+                    Console.WriteLine();
+                }
+
+
+
+                //foreach (var row in reader)
+                //{
+                //    string rowValue = row.ToString();
+                //    Console.WriteLine(rowValue);
+                //}
             }
         }
         
         catch(Exception ex)
         {
-
+            Console.WriteLine($"Exception throw: {ex.ToString()}");
         }
-        finally
-        {
-            if (connection != null)
-            {
-                connection.Close();
-            }
-        }
+        //finally
+        //{
+        //    if (connection != null && connection.State == ConnectionState.Open)
+        //    {
+        //        connection.Close();
+        //    }
+        //}
 
         Console.ReadLine();
+    }
+
+
+    static void GetSchemaInfo(SQLiteConnection connection, string sqlQuery)
+    {
+        //using (connection)
+        //{
+            SQLiteCommand command = new SQLiteCommand(
+                                                      sqlQuery,
+                                                      connection);
+            //connection.Open();
+
+            SQLiteDataReader reader = command.ExecuteReader();
+            DataTable schemaTable = reader.GetSchemaTable();
+
+            foreach (DataRow row in schemaTable.Rows)
+            {
+                Console.WriteLine("New Data Row \r\n");
+                foreach (DataColumn column in schemaTable.Columns)
+                {
+                    Console.WriteLine(string.Format("{0} = {1}",
+                       column.ColumnName, row[column]));
+                }
+            }
+        //}
     }
 }
